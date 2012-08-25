7c4cb4e061a6c6a64c5c2832b3c986c5c5f32899
Spun <neoappengine@gmail.com>
2012-08-25 18:04:19 +0200

    Eliminado uso de EventEmitter
    -Eliminada la dependencia de EventEmitter para el paso de tweets entre
    el requets del stream y el emit del socket.
    -Eliminado log de error del index.html
    -Eliminado fichero package.json

--- server.js
+++ server.js
@@ -1,53 +1,42 @@
 var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , https = require('https')
-  , events = require("events")
-  , fs = require('fs')
+	, io = require('socket.io').listen(app)
+	, https = require('https')
+	, fs = require('fs');
 
 app.listen(8080);
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
+	fs.readFile(__dirname + '/index.html',
+	function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
 }
 
 
-var tweet_emitter = new events.EventEmitter();
-
 // Twitter stream options
 var options = {
-  host: 'stream.twitter.com',
-  port: 443,
-  path: '/1/statuses/filter.json?track=[topic]',  // ### topic to stream
-  method: 'GET',
-  auth: '[user]:[pass]'	// ### twitter user:pass
+	host: 'stream.twitter.com',
+	port: 443,
+	path: '/1/statuses/filter.json?track=[topic]',  // ### topic to stream
+	method: 'GET',
+	auth: '[user]:[pass]'	// ### twitter user:pass
 };
-
-// Twitter stream request
-var req = https.request(options, function(res) {
-	res.on('data', function(response) {
-		var datos = response.toString('utf8');
-		tweet_emitter.emit("tweets", datos);			//socket.emit('yeyy',datos);
-	});
-});
-req.end();
-
 
 // Sockets
 io.configure(function () {
   io.set('log level', 0);
 });
 
-io.sockets.on('connection', function (socket) {
-	tweet_emitter.addListener("tweets", function(tweet) {
-		socket.volatile.emit('new tweet', tweet);
+// Twitter stream request
+var req = https.request(options, function(res) {
+	res.on('data', function(response) {
+		var datos = response.toString('utf8');
+		io.sockets.volatile.emit('new tweet', datos);
 	});
 });
+req.end();
